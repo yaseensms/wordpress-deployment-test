f43625aa60c648c33f3b223cfd1c023fcdf3ef6b
yaseensms <104913213+yaseensms@users.noreply.github.com>
2023-08-20 18:11:00 +0200

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ensure the script stops on first error
+set -e
+
 # Database credentials
 USER="wp_admin"
 PASSWORD="Wp3#Secure$Pass123"
@@ -8,13 +11,23 @@ HOST="wordpress-db-instance.cfshqvbbbgj3.eu-central-1.rds.amazonaws.com"
 PORT=3306
 
 # Backup directory
-BACKUP_DIR="/path/to/backup/directory"
+BACKUP_DIR="/var/backups"
+
+# Check if the backup directory exists, if not create it
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p "$BACKUP_DIR"
+fi
 
 # Date format for backup file
 DATE=$(date +%d-%m-%Y_%H-%M-%S)
 
 # Create backup
-mysqldump -h $HOST -P $PORT -u $USER -p$PASSWORD $DATABASE > $BACKUP_DIR/db_backup_$DATE.sql
+if mysqldump -h $HOST -P $PORT -u $USER -p$PASSWORD $DATABASE > $BACKUP_DIR/db_backup_$DATE.sql; then
+    echo "Backup completed!"
+else
+    echo "Backup failed!" >&2
+    exit 1
+fi
 
-echo "Backup completed!"
-
+# Compress the backup for storage efficiency
+gzip $BACKUP_DIR/db_backup_$DATE.sql
